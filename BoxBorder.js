cd9ceac5a4ec703b73ed9fc66cfc28becae23189
ilisha0512 <68852416+ilisha0512@users.noreply.github.com>
2020-08-11 19:38:59 +0530

    Add files via upload

---
+++ BoxBorder.js
@@ -0,0 +1,23 @@
+class BoxBorder {
+    constructor(x, y, width, height) {
+        var options = {
+            restitution: 1
+        }
+        this.body = Bodies.rectangle (x,y,width,height, options);
+        this.width = width;
+        this.height = height;
+        World.add (world, this.body);
+    }
+    display () {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push()
+        translate(pos.x, pos.y);
+        angleMode(RADIANS);
+        rotate(angle);
+        rectMode(CENTER);
+        fill ("Red");
+        rect(0, 0, this.width, this.height);
+        pop()
+    }
+}
